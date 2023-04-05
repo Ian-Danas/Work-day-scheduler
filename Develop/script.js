f134af95aa9fd9ed6110c4d213b152fbe0551723
Ian Danas <ianmdanas@gmail.com>
2023-04-04 19:03:26 -0700

    adding code to save text entry into local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,15 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+localStorage.setItem('hour-9','')
+localStorage.setItem('hour-10','')
+localStorage.setItem('hour-11','')
+localStorage.setItem('hour-12','')
+localStorage.setItem('hour-1','')
+localStorage.setItem('hour-2','')
+localStorage.setItem('hour-3','')
+localStorage.setItem('hour-4','')
+localStorage.setItem('hour-5','')
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -9,7 +18,16 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   var saveBtn = $('.saveBtn')
-  saveBtn.on('click',saveEvent)
+  saveBtn.on('click',function(event){
+    var parentDiv = event.target.parentElement
+    console.log('parent element',parentDiv)
+    parentDiv = $(parentDiv)
+    var hourNum  = parentDiv.attr('id')
+    console.log('hour id',hourNum)
+    event2Save = parentDiv.children('textarea').val()
+    console.log('text in area',event2Save)
+    localStorage.setItem(hourNum,event2Save)
+  })
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -26,13 +44,14 @@ $(function () {
   $('#currentDay').text(today.format('dddd, MMMM D'))
 });
 
-function saveEvent(event){
-  parentDiv = event.target.parentElement
-  parentDiv = $(parentDiv)
-  event2Save = parentDiv.children('textarea').value
-  console.log(event2Save)
-  // var event2store = parentDiv.children('<textarea>')
-  // console.log(event2store)
 
 
-}
+// function saveEvent(event){
+//   var parentDiv = event.target.parentElement
+//   parentDiv = $(parentDiv)
+//   var hourNum  = parentDiv.attr('id')
+//   console.log(hourNum)
+//   event2Save = parentDiv.children('textarea').val()
+//   console.log(event2Save)
+//   localStorage.setItem(hourNum,event2Save)
+// }
