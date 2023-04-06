f5b620345d24f1a833db11e8c66b40e58d496b18
Ian Danas <ianmdanas@gmail.com>
2023-04-06 16:55:27 -0700

    refactoring code to be simpler and easier to read and fixing comments in js code

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,9 @@
 // in the html.
 
 $(function () {
-  // Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // Added a listener for click events on the save button. This code uses
+  // the id in the containing time-block as a key to save the user input in
+  // local storage. 
   var saveBtn = $('.saveBtn')
 
   saveBtn.on('click',function(event){
@@ -18,55 +15,48 @@ $(function () {
   })
 
   //
-  // Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour.
+  var timeBlocks = $('.time-block')
   function updatePlannerColor(){
-    for (let i = 9; i <= 17; i++) {
+    for (let i = 0; i < timeBlocks.length; i++) {
       var currentTime = dayjs().hour()
-      console.log(currentTime)
-      var hour = '#hour-' + i
-      var hourEl = $(hour)
-      if (i < currentTime){
-        hourEl.removeClass('present')
-        hourEl.removeClass('future')
-        hourEl.addClass('past')
-      }if (i == currentTime){
-        hourEl.removeClass('past')
-        hourEl.removeClass('future')
-        hourEl.addClass('present')
-      }if(i > currentTime){
-        hourEl.removeClass('past')
-        hourEl.addClass('present')
-        hourEl.addClass('future')
+      var element = $(timeBlocks[i])
+      var hourEl = element.attr('id')
+      var hour = hourEl.split('-')[1]
+      if (hour < currentTime){
+        element.removeClass('present')
+        element.removeClass('future')
+        element.addClass('past')
+      }if (hour == currentTime){
+        element.removeClass('past')
+        element.removeClass('future')
+        element.addClass('present')
+      }if(hour > currentTime){
+        element.removeClass('past')
+        element.addClass('present')
+        element.addClass('future')
       }
-      console.log(hourEl)
       
     }
 
   }
-   setInterval(updatePlannerColor,60000)
+   updatePlannerColor()
 
   //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. 
 function updatePlannerText(){
-  for (let i = 9; i <= 17; i++) {
-    var currentTime = dayjs().hour()
-    var hour = 'hour-' + i
-    var hourEl = $('#' +hour)
-    var plannerText = hourEl.children('.description')
+  for (let i = 0; i <= timeBlocks.length; i++) {
+    var element = $(timeBlocks[i])
+    var hour = element.attr('id')
+    var plannerText = element.children('.description')
     var storedVal = localStorage.getItem(hour)||''
     plannerText.text(storedVal)
   }
 }
 updatePlannerText()
-// setInterval(updatePlannerText,1000)
-
-  // Add code to display the current date in the header of the page.
+  // code to display the current date in the header of the page.
   var today = dayjs()
   $('#currentDay').text(today.format('dddd, MMMM D'))
 
